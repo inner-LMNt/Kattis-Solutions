65f311b6062ea8ef19f56c1d70d4b9e9225d17f7
Kevin Huang <huan2005@purdue.edu>
2024-08-02 04:44:20 -0400

    Topic 4 (1/2)

---
+++ divide-and-conquer/ljutnja.py
@@ -0,0 +1,101 @@
+def ljutnja_binary_search():
+    candies, n = map(int, input().split())
+
+    children = []
+    for i in range(n):
+        children.append(int(input()))
+
+    children.sort(reverse=True) # most greedy children first
+
+    higher = children[0]
+    lower = 0
+    candies_distributed = 0
+    max_height = 0
+
+    # Binary search for the maximum height remaining after optimal distribution
+    while lower < higher:
+        middle = (lower + higher) // 2
+        candies_distributed = 0
+        max_height = children[0] - middle
+
+        for i in range(n): # distribute until each child is at most `max_height`
+            if children[i] <= max_height:
+                break
+            candies_distributed += children[i] - max_height
+
+        if candies_distributed <= candies:
+            lower = middle + 1
+        else:
+            higher = middle
+
+    # Found the max height where minimal candies remaining after distribution
+    max_height = children[0] - lower + 1
+
+    # First distribute until `max_height`
+    candies_distributed = 0
+    for i in range(n):
+        if children[i] <= max_height:
+            break
+        candies_distributed += children[i] - max_height
+
+    candies_left = candies - candies_distributed
+
+    # Calculate anger while distributing remaining candies
+    sum_of_squares = 0
+    for i in range(n):
+        if children[i] >= max_height:
+            if candies_left > 0:
+                sum_of_squares += (max_height - 1) * (max_height - 1)
+                candies_left -= 1
+            else:
+                sum_of_squares += (max_height) * (max_height)
+        else:
+            sum_of_squares += children[i] * children[i]
+
+    print(sum_of_squares)
+
+
+def ljutnja_greedy():
+    candies, n = map(int, input().split())
+
+    children = []
+    for i in range(n):
+        children.append(int(input()))
+
+    children.sort(reverse=True) # most greedy children first
+
+    index = 0
+    height = children[0]
+
+    # Because anger levels are squared, we want to distribute as evenly as possible
+    # Think of distributing by layer, cutting slices off the top of a pyramid of values
+    while candies > 0:
+        while index < n and children[index] == height: # find next layer width
+            index += 1
+
+        if candies < index: # not enough for full layer
+            break
+
+        candies -= index
+        height -= 1
+        
+    for i in range(index): # take slicings into effect
+        children[i] = height
+
+    for i in range(candies): # distribute remaining candies
+        children[i] -= 1
+
+    sum_of_squares = 0
+    for i in range(n):
+        sum_of_squares += children[i] * children[i]
+
+    print(sum_of_squares)
+
+
+from random import randint
+
+# Either solution works
+if randint(0, 1) == 0:
+    ljutnja_binary_search()
+else:
+    ljutnja_greedy()
---
+++ divide-and-conquer/svada.py
@@ -0,0 +1,63 @@
+time = int(input())
+
+N = int(input())
+monkeys_A = []
+for i in range(N):
+    monkeys_A.append(input().split())
+
+M = int(input())
+monkeys_B = []
+for i in range(M):
+    monkeys_B.append(input().split())
+
+upper_time = time
+lower_time = 0
+A_time = 0
+
+# Because we can quickly verify a potential time, use BSTA
+while lower_time < upper_time:
+    A_time = (lower_time + upper_time) // 2
+    B_time = time - A_time
+    coconuts = 0
+
+    # Calculate how many coconuts are gathered by all A-type monkeys
+    for monkey in monkeys_A:
+        if A_time >= int(monkey[0]):
+            coconuts += 1 + (A_time - int(monkey[0])) // int(monkey[1])
+
+    # Calculate how many coconuts are opened by all B-type monkeys
+    for monkey in monkeys_B:
+        if B_time >= int(monkey[0]):
+            coconuts -= 1 + (B_time - int(monkey[0])) // int(monkey[1])
+
+    if coconuts > 0: # too many coconuts remaining
+        upper_time = A_time
+    elif coconuts < 0: # too few
+        lower_time = A_time + 1
+    else:
+        break
+
+result = 0
+excess = 1000000000
+mid_time = (lower_time + upper_time) // 2
+
+# After breaking from BSTA, check `mid_time-1` and `mid_time`
+# This is because using `mid_time` can sometimes result in too many net coconuts
+for i in range(mid_time - 1, mid_time + 1):
+    A_time = i
+    B_time = time - i
+    coconuts = 0
+
+    for monkey in monkeys_A:
+        if A_time >= int(monkey[0]):
+            coconuts += 1 + (A_time - int(monkey[0])) // int(monkey[1])
+
+    for monkey in monkeys_B:
+        if B_time >= int(monkey[0]):
+            coconuts -= 1 + (B_time - int(monkey[0])) // int(monkey[1])
+
+    # Valid net coconuts and minimum excess
+    if coconuts <= 0 and -coconuts < excess:
+        result = A_time
+
+print(result)
